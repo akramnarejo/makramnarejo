7089f54b09ede9cfabe1ed6411c7d0d1605c9552
Akram Narejo <akramnarejo@Akrams-MacBook-Pro.local>
2025-05-03 12:38:52 +0400

    fix: font weight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,72 +4,6 @@ import { FiExternalLink, FiArrowRight } from "react-icons/fi";
 
 import { projects } from "./lib/constants";
 
-
-// export default function Home() {
-//   return (
-//     <main>
-//       <section>
-//         <h1 className="[font-size:_clamp(2rem,5vw,3rem)] font-bold my:2 lg:my-4">
-//           G&apos;day, I&apos;m Akram <span className="animate-pulse duration-1000">👋</span>
-//         </h1>
-//         <div className="w-full flex flex-col gap-4 font-normal [font-size:_clamp(1.2rem,4vw,2.5rem)] mt-1  text-[#666]">
-
-//         <p className="">
-//           A{" "}<Link href="/work" className="dark:bg-[#111] bg-[#eee] rounded-lg px-1 border border-[#333] text-blue-800 hover:text-blue-500 ease-linear duration-500">Frontend Engineer 👨‍💻</Link>{" "}
-//           passionate about crafting seamless user experience UI/UX.
-//             Leveraging modern technology, I deliver products that resonate with
-//             users and drive results for businesses.
-//         </p>
-//         <p className="">
-//             I work at{" "}
-//             <Link
-//               href={"https://schoolhack.ai/"}
-//               className="hover:underline underline-offset-2 cursor-pointer hover:text-blue-500 dark:text-[#999999] text-[#222222]"
-//             >
-//               School Hack
-//             </Link>
-//             {", "}developing{" "}
-//             <Link
-//               href="https://on-demand.io"
-//               className=" hover:text-blue-500 dark:text-[#999999] text-[#222222]"
-//             >
-//               On-Demand
-//             </Link>
-//             {": "}
-//             a cutting-edge AI Platform as a Service (PaaS) powered by RAG AI technology.
-//           </p>
-//         <p className="">Web3 enthusiast for better tomorrow and Hobbyist photographer 📸.</p>
-//         </div>
-        
-//       </section>
-//       <section>
-//       <p className="w-full mt-2 tracking-wide text-[#666] italic [font-size:_clamp(1.2rem,1.5vw,2rem)]">
-//           For contact, send me an email
-//           <Link
-//             href="mailto:makramnarejo@gmail.com"
-//             className="hover:underline text-blue-600"
-//           >
-//             &nbsp;here
-//           </Link>
-//           .
-//         </p>
-//       <div className="flex gap-4 box-border mt-4">
-//         <Link href='https://linkedin.com/in/akramnarejo' target="_blank" className="text-2xl md:text-3xl text-[#333] dark:text-[#999] hover:text-blue-500 duration-500">
-//           <FaLinkedin />
-//         </Link>
-//         <Link href='https://github.com/akramnarejo' target="_blank" className="text-2xl md:text-3xl text-[#333] dark:text-[#999] hover:text-gray-500 duration-500">
-//           <FaGithub />
-//         </Link>
-//         <Link href='https://instagram.com/akramnarejo' target="_blank" className="text-2xl md:text-3xl text-[#333] dark:text-[#999] hover:text-pink-600 duration-500">
-//           <FaInstagram />
-//         </Link>
-//       </div>
-//       </section>
-      
-//     </main>
-//   );
-// }
-
 export default function Home() {
   return (
     <main className="max-w-screen-xl mx-auto px-4 py-8 space-y-12 md:space-y-16 lg:space-y-24">
@@ -84,7 +18,7 @@ export default function Home() {
             </p>
           </div>
 
-          <div className="space-y-4 animate-fade-in animation-delay-400 text-base sm:text-xl lg:text-2xl relative z-10 max-w-4xl">
+          <div className="space-y-4 animate-fade-in animation-delay-400 font-light text-base sm:text-xl lg:text-2xl relative z-10 max-w-4xl">
             <p className="text-gray-600 dark:text-gray-300">
               Currently, working at{" "}
               <Link href="https://schoolhack.ai/" className="text-blue-600 dark:text-blue-400 hover:underline">
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -10,26 +10,26 @@ const Page = () => {
       <h2 className="[font-size:_clamp(2rem,3vw,6rem)] font-bold dark:text-white">
         Work
       </h2>
-      <p className="text-gray-600 dark:text-gray-300 [font-size:_clamp(1.2rem,1.5vw,2rem)]">
+      <p className="text-gray-600 font-light dark:text-gray-300 [font-size:_clamp(1.2rem,1.5vw,2rem)]">
         For the past 5 years developing mobile responsive and scalable web apps, providing seamless user experience using modern technologies. So far, I have worked with the following skills and learning more.
       </p>
       {/* skills */}
-      <div className="flex gap-4 flex-wrap mt-8">
+      <ul className="flex gap-4 flex-wrap mt-8">
         {skills.map((item, ind) => {
           return (
-            <motion.span
+            <motion.li
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
               transition={{ duration: 2, type: "sping", ease: "easeOut" }}
               viewport={{once: true}}
               key={ind}
-              className={`md:text-xl dark:bg-black border dark:border-[#444] border-[#999] dark:hover:border-blue-200 dark:hover:text-blue-200 hover:border-blue-400 hover:text-blue-600 p-2 rounded-lg text-gray-600 dark:text-gray-300 hover:skew-y-3 ease-in-out duration-500 hover:scale-110`}
+              className={`font-light md:text-xl dark:bg-black border dark:border-[#444] border-[#999] dark:hover:border-blue-200 dark:hover:text-blue-200 hover:border-blue-400 hover:text-blue-600 p-2 rounded-lg text-gray-600 dark:text-gray-300 hover:skew-y-3 ease-in-out duration-500 hover:scale-110`}
             >
               {item}
-            </motion.span>
+            </motion.li>
           );
         })}
-      </div>
+      </ul>
 
       {/* Projects */}
       <h2 className="mt-16 mb-2 text-4xl font-semibold">Projects</h2>
