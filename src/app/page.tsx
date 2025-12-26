25cf835b8b332b2d3c92398cfda912a9710c8068
Muhammad Akram <makramnarejo@gmail.com>
2025-12-26 10:27:10 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
           <div className="space-y-4 animate-fade-in animation-delay-400 font-light text-base sm:text-xl lg:text-2xl relative z-10 max-w-4xl">
             <p className="text-gray-600 dark:text-gray-300">
               Currently, working at{" "}
-              <Link href="https://schoolhack.ai/" className="text-blue-600 dark:text-blue-400 hover:underline">
-                School Hack
+              <Link href="https://airev.ai/" className="text-white bg-purple-600 px-4 py-1 hover:underline">
+                Airev
               </Link>
               , developing{" "}
               <Link href="https://on-demand.io" className="text-blue-600 dark:text-blue-400 hover:underline">
